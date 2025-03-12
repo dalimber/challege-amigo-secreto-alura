cb2d5a659c5a6b1cceafcaf3ac2823da93399a73
dalimberAndrade <dalimberpubg@gmail.com>
2025-03-12 17:47:23 -0500

    Crea el Array y crear funcion para agregar nombre a array

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Crear Array para almacenar nombres
+let nombreAmigos = [];
+//Crear funcion para almacenar los nombres en el Array nombreAmigos
+function agregarAmigo()
+{
+//Capturamos nombre
+let nombreAmigo = document.getElementById('amigo').value;
+console.log('Nombre digitado:', nombreAmigo);
+//Agregamos nombre a Array
+nombreAmigos.push(`${nombreAmigo}`);
+console.log(nombreAmigos.length);
+
+
+}

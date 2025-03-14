6c875ec411dfa8e1e5c763889b614b287cfe6bb7
dalimberAndrade <dalimberpubg@gmail.com>
2025-03-14 13:38:16 -0500

    Se creo una funcion para mostrar elementos en los espacios de listas desiganados, una funcion para limpiar las listas, se creo la funcion que sortea el nombre y lo muestra en lista

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ console.log('Nombre digitado:', nombreAmigo);
  selectInput.value='';
  console.log(selectInput);
  selectInput.select();
- actualizarLista(nombreAmigos);
+ actualizarLista();
  }
  else
  {
@@ -26,26 +26,64 @@ console.log('Nombre digitado:', nombreAmigo);
  }
 }
 //Crear funcion para actualizar lista de amigos
-function actualizarLista(nombreAmigos) 
+function actualizarLista() 
 {
+let idElemento='listaAmigos';
+limpiarListas(idElemento);
 //Selecciono el elemento ul o lista
-var listado =document.getElementById('listaAmigos');
+//var listado =document.getElementById('listaAmigos');
 //Limpio la listapara que no se repitan elementos en pantalla
-listado.innerHTML = "";
+//listado.innerHTML = "";
 //Ciclo para recorrer array y mostrar elementos en lista
-for (let inicio = 0; inicio < nombreAmigos.length; inicio++) 
+for (let posicion = 0; posicion < nombreAmigos.length; posicion++) 
  {
+ mostrarElementos(idElemento,nombreAmigos,posicion);
  //Comprobacion de numeros seleccionados
- console.log(inicio);
+ //console.log(inicio);
  //Creamos elemento li
- var agregarListado=document.createElement("li");
+ //var agregarListado=document.createElement("li");
  //Creamos variable para crear texto que alamacene el nombre
- var nombre=document.createTextNode(nombreAmigos[inicio])
+ //var nombre=document.createTextNode(nombreAmigos[inicio]);
  //Comprobamos nombre
- console.log(nombreAmigos[inicio])
+ //console.log(nombreAmigos[inicio]);
  //Agregamos el nombre a el elemneto li
- agregarListado.appendChild(nombre);
+ //agregarListado.appendChild(nombre);
  //Agregamos el li a la lista ul
- listado.appendChild(agregarListado);
+ //listado.appendChild(agregarListado);
+
  }
+}
+//Crear la funcion para selecionar aleatoriamente un nombre del array
+function sortearAmigo() 
+{
+//Creamos el selecionador aleatorio mediante un numero
+let eleccion = Math.floor(Math.random()*nombreAmigos.length);  
+//Verificamos numero y nombre selecionado
+console.log(`Numero:${eleccion}, Nombre: ${nombreAmigos[eleccion]}`);
+//Mostramos el nombre selecionado en pantalla
+let idElemento = 'resultado';
+limpiarListas(idElemento);
+mostrarElementos(idElemento,nombreAmigos,eleccion);
+}
+//Funcion para crear elementos li
+function mostrarElementos(elementId,lista,posicion) 
+{
+    //Seleccionamos el espacio para la lista o ul
+    var espacioLista = document.getElementById(`${elementId}`);
+    //Creamos elemento li
+    var agregarLi = document.createElement('li');
+    //Creamos el almacenamiento del texto
+    var crearTexto = document.createTextNode(lista[posicion]);
+    //Agregamos el texto al elemento li
+    agregarLi.appendChild(crearTexto);
+    //Agregamos el elemento li a elemento ul
+    espacioLista.appendChild(agregarLi);
+
+}
+//Funcion para limpiar los elementosul de listas
+function limpiarListas(elementId) 
+{
+    //Seleccionamos el espacio para la lista o ul
+    var espacioLista = document.getElementById(`${elementId}`);
+    espacioLista.innerHTML = "";
 }
